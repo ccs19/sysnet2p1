81bca1f0e6ef0bc8509f02c15065f7c76951a9cb
Brett Rowberry <brett.rowberry@gmail.com>
2015-01-22 18:55:44 -0600

    Created tester file for TCPclient.c, TCPmain.c, which is required in the project write-up.

---
+++ TCPClient/TCPmain.c
@@ -0,0 +1,13 @@
+/*
+ * TCPmain.c
+ * Systems and Networks II
+ * Project 1
+ * Christopher Schneider & Brett Rowberry
+ *
+ * This is a tester file for TCPClient.c
+ */
+
+int main()
+{
+    return 0;
+}
